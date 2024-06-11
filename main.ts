a2b393bf7c1584e7076c19bf2d7c4b71ef391996
data-jeong <lightyear94122@gmail.com>
2024-06-11 12:33:31 +0900

    NoAngleBrackets

--- main.ts
+++ main.ts
@@ -2,138 +2,54 @@ import {
 	App,
 	Editor,
 	MarkdownView,
-	Modal,
-	Notice,
 	Plugin,
 	PluginSettingTab,
 	Setting,
 } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: "default",
-};
-
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
+		// 리본 아이콘 추가
+		this.addRibbonIcon(
+			"wrench",
+			"Toggle Angle Brackets",
 			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("This is a notice!");
+				// 버튼 클릭 시 실행되는 코드
+				this.toggleAngleBrackets();
 			}
 		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
-		});
-
-		this.addCommand({
-			id: "wrap-angle-brackets",
-			name: "Wrap angle brackets with backticks",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				const selection = editor.getSelection();
-				const wrappedText = selection
-					.replace(/</g, "`<")
-					.replace(/>/g, ">`");
-				editor.replaceSelection(wrappedText);
-			},
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
+		// 세팅 탭 추가
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
 	}
 
-	onunload() {}
-
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
+	toggleAngleBrackets() {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (activeView) {
+			const editor = activeView.editor;
+			const selection = editor.getSelection();
+			const lines = selection.split("\n");
+			let insideCodeBlock = false;
+			const processedLines = lines.map((line) => {
+				if (line.startsWith("```")) {
+					// 코드 블록 시작 또는 끝
+					insideCodeBlock = !insideCodeBlock;
+					return line;
+				} else if (insideCodeBlock) {
+					// 코드 블록 내부이면 변경하지 않음
+					return line;
+				} else {
+					// 코드 블록 외부이면 '<'와 '>'를 백틱으로 감싸거나 백틱을 제거함
+					if (line.includes("`<") || line.includes(">`")) {
+						return line.replace(/`</g, "<").replace(/>`/g, ">");
+					} else {
+						return line.replace(/</g, "`<").replace(/>/g, ">`");
+					}
+				}
+			});
+			const processedText = processedLines.join("\n");
+			editor.replaceSelection(processedText);
+		}
 	}
 }
 
@@ -151,16 +67,14 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
+			.setName("Toggle angle brackets")
+			.setDesc(
+				"Click the button to toggle backticks around angle brackets in the selected text (outside code blocks)."
+			)
+			.addButton((button) => {
+				button.setButtonText("Toggle").onClick(async () => {
+					this.plugin.toggleAngleBrackets();
+				});
+			});
 	}
 }
